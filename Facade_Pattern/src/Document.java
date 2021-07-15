e63aaac2bfb02377683d2c480c62b3e9a01a555e
Merphous Liu <merphousliu@gmail.com>
2021-07-14 21:10:00 -0400

    //added methods

--- Facade_Pattern/src/Document.java
+++ Facade_Pattern/src/Document.java
@@ -1,4 +1,7 @@
+import java.util.List;
 
 public abstract class Document {
 	public abstract void description();
+	public abstract void save(List<String> programData);	
+	public abstract List<String> load();
 }
